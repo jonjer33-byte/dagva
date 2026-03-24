bb8617d7aa6f2f8d911232fa1549477c3f54ba8f
jonjer33-byte <jonjer33@gmail.com>
2026-03-23 20:02:35 -0500

    Create configuration constants file

---
+++ config/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+// Configuration Constants
+
+define('APP_ENV', 'production');
+define('APP_DEBUG', false);
+define('APP_KEY', 'your_application_key');
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_DATABASE', 'your_database');
+define('DB_USERNAME', 'your_username');
+define('DB_PASSWORD', 'your_password');
+
+?>
